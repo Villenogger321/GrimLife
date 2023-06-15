1415cf589707f329ade41ec12db5c04e1f0f524b
isac <isahal96@live.com>
2023-06-15 17:24:27 +0200

    s\dgf
    asd

---
+++ GrimLife/Assets/GrimLife/Scenes/trash.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.LoadLevel(1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
